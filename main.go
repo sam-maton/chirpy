71bdabc035ef4196a3a4de83ba1ba0f61f548906
Sam Maton <sam.maton91@gmail.com>
2024-12-09 11:47:23 +0000

    setup handler for health scan

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := http.Server{
 		Handler: mux,
